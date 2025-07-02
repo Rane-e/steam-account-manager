01aadd0446f5c39d34d09b5e847d631de3b57510
rane <mashtarov.wf@mail.ru>
2025-07-02 11:38:40 +0700

    (feature): add logging for SteamAccount resource

--- backend/src/steam-account/steam-account.resolver.ts
+++ backend/src/steam-account/steam-account.resolver.ts
@@ -3,26 +3,34 @@ import { SteamAccountService } from './steam-account.service';
 import { SteamAccount } from './entities/steam-account.entity';
 import { CreateSteamAccountInput } from './dto/create-steam-account.input';
 import { UpdateSteamAccountInput } from './dto/update-steam-account.input';
+import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 
 @Resolver(() => SteamAccount)
 export class SteamAccountResolver {
-  constructor(private readonly steamAccountService: SteamAccountService) {}
+  constructor(
+    private readonly steamAccountService: SteamAccountService,
+    @InjectPinoLogger(SteamAccountService.name)
+    private readonly logger: PinoLogger,
+  ) {}
 
   @Mutation(() => SteamAccount)
   createSteamAccount(
     @Args('createSteamAccountInput')
     createSteamAccountInput: CreateSteamAccountInput,
   ) {
+    this.logger.debug('GraphQL mutation createSteamAccount');
     return this.steamAccountService.create(createSteamAccountInput);
   }
 
   @Query(() => [SteamAccount], { name: 'steamAccount' })
   findAll() {
+    this.logger.debug('GraphQL query findAll');
     return this.steamAccountService.findAll();
   }
 
   @Query(() => SteamAccount, { name: 'steamAccount' })
   findOne(@Args('uuid', { type: () => String }) uuid: string) {
+    this.logger.debug('GraphQL query findOne');
     return this.steamAccountService.findOne(uuid);
   }
 
@@ -31,6 +39,7 @@ export class SteamAccountResolver {
     @Args('updateSteamAccountInput')
     updateSteamAccountInput: UpdateSteamAccountInput,
   ) {
+    this.logger.debug('GraphQL mutation updateSteamAccount');
     return this.steamAccountService.update(
       updateSteamAccountInput.uuid,
       updateSteamAccountInput,
@@ -39,6 +48,7 @@ export class SteamAccountResolver {
 
   @Mutation(() => SteamAccount)
   removeSteamAccount(@Args('uuid', { type: () => String }) uuid: string) {
+    this.logger.debug('GraphQL mutation removeSteamAccount');
     return this.steamAccountService.remove(uuid);
   }
 }
--- backend/src/steam-account/steam-account.service.ts
+++ backend/src/steam-account/steam-account.service.ts
@@ -2,29 +2,50 @@ import { Injectable } from '@nestjs/common';
 import { CreateSteamAccountInput } from './dto/create-steam-account.input';
 import { UpdateSteamAccountInput } from './dto/update-steam-account.input';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
+import { SteamAccount } from '@prisma/client';
 
 @Injectable()
 export class SteamAccountService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    @InjectPinoLogger(SteamAccountService.name)
+    private readonly logger: PinoLogger,
+  ) {}
 
-  create(createSteamAccountInput: CreateSteamAccountInput) {
-    return this.prisma.steamAccount.create({ data: createSteamAccountInput });
+  async create(dto: CreateSteamAccountInput) {
+    this.logger.info({ dto }, 'Creating email account');
+    const entity = await this.prisma.steamAccount.create({ data: dto });
+    this.logger.debug({ entity }, 'Created email account');
+    return entity;
   }
 
   findAll() {
+    this.logger.debug('Listing all steam accounts');
     return this.prisma.steamAccount.findMany();
   }
 
-  findOne(uuid: string) {
-    return this.prisma.steamAccount.findUnique({ where: { uuid } });
+  async findOne(uuid: string): Promise<SteamAccount | null> {
+    this.logger.info({ uuid }, 'Lookup steam account');
+    const entity = await this.prisma.steamAccount.findUnique({
+      where: { uuid },
+    });
+
+    if (!entity) {
+      this.logger.warn({ uuid }, 'Steam account not found');
+    }
+
+    return entity;
   }
 
-  update(uuid: string, updateSteamAccountInput: UpdateSteamAccountInput) {
-    const { uuid: _omit, ...data } = updateSteamAccountInput;
+  update(uuid: string, dto: UpdateSteamAccountInput) {
+    this.logger.info({ uuid, dto }, 'Steam email account');
+    const { uuid: _omit, ...data } = dto;
     return this.prisma.steamAccount.update({ where: { uuid }, data });
   }
 
   remove(uuid: string) {
+    this.logger.warn({ uuid }, 'Deleting steam account');
     return this.prisma.steamAccount.delete({ where: { uuid } });
   }
 }
