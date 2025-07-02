3cd094e87edeedb54ace28ac4bae01494d04edd1
rane <mashtarov.wf@mail.ru>
2025-07-02 09:23:01 +0700

    (init): GraphQL & ConfigModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,22 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      introspection: process.env.NODE_ENV !== 'production',
+      graphiql: process.env.NODE_ENV !== 'production',
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [AppResolver],
 })
 export class AppModule {}
